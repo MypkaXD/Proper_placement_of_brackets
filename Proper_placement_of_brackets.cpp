772c9a00c46ef24139e3478755ab80ac245f8f7f
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-10-17 22:49:36 +0300

    final

---
+++ Proper_placement_of_brackets.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stack>
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "rus");
+
+	std::stack <char> steck;
+
+	for (int count = 0; argv[1][count] != '\0'; count++) {
+		switch (argv[1][count])
+		{
+		case ('('): steck.push(')'); break;
+		case ('['): steck.push(']'); break;
+		case ('{'): steck.push('}'); break;
+		case ('<'): steck.push('>'); break;
+
+		case (']'):
+		case ('}'):
+		case ('>'):
+		case (')'):
+			if (steck.empty() || steck.top() != argv[1][count]) {
+				std::cout << "FALSE";
+				exit(0);
+			}
+			steck.pop();
+		default:
+			break;
+		}
+	}
+	if (steck.empty())
+		std::cout << "TRUE";
+	else
+		std::cout << "FALSE";
+
+	return 0;
+}
---
+++ script.bat
@@ -0,0 +1,2 @@
+cmd /c start "" /d "C:\Users\Mypka\Downloads" /b Proper_placement_of_brackets "(a+b)=c{sss}{}{}{{}}"
+pause
